92e4dee23c44a5cec9dbf3a33982e60d783279d6
Aditya Ingole <aditayingole113@gmail.com>
2025-07-07 15:49:53 +0530

    initial commit

---
+++ src/BlockEstate.sol
@@ -0,0 +1,13 @@
+SPDX license indentifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"
+import "@openzeppelin/contracts/access/Accesscontrol.sol";
+
+
+contract BlockEstate is ERC721URIStorage ,Accesscontrol {
+    
+
+
+}
+  
