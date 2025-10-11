fe6ab6eb034454366818f714cca729c0566972a8
Aditya-ingole21 <aditayingole113@gmail.com>
2025-10-11 16:54:41 +0530

    the property token is completed

---
+++ src/PropertyToken.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PropertyToken is ERC721, Ownable {
+    uint256 public tokenCounter;
+    mapping(uint256 => string) public propertyMetadata; // IPFS hash for metadata
+
+    constructor() ERC721("BlockEstate", "BEST") Ownable(msg.sender) {
+        tokenCounter = 0;
+    }
+
+    // Mint a new property NFT
+    function mintProperty(address owner, string memory ipfsHash) public onlyOwner returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(owner, newTokenId);
+        propertyMetadata[newTokenId] = ipfsHash;
+        tokenCounter++;
+        return newTokenId;
+    }
+
+    // Get metadata for a property
+    function getPropertyMetadata(uint256 tokenId) public view returns (string memory) {
+        require(_exists(tokenId), "Token does not exist");
+        return propertyMetadata[tokenId];
+    }
+
+    // Update metadata (e.g., for document updates)
+    function updateMetadata(uint256 tokenId, string memory newIpfsHash) public onlyOwner {
+        require(_exists(tokenId), "Token does not exist");
+        propertyMetadata[tokenId] = newIpfsHash;
+    }
+}
